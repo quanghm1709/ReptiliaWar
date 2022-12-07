a0ead22774136af6c5e96fc22c047cace96003be
quanghm201 <haminhquang201@gmail.com>
2022-12-07 11:08:12 +0700

    Add arrow to ballista weap

--- Assets/Scripts/Core/Core.cs
+++ Assets/Scripts/Core/Core.cs
@@ -6,31 +6,32 @@ using UnityEngine.UI;
 public abstract class Core : MonoBehaviour
 {
     [Header("Data Core")]
-    [SerializeField] public string characterName;
-    [SerializeField] public int maxHp;
-    [SerializeField] public int maxAtk;
-    [SerializeField] public bool isOwner;
-    [SerializeField] public bool canMove = true;
-    [SerializeField] public bool isBuilding;
-    [SerializeField] public bool isRanger;
+    public string characterName;
+    public int maxHp;
+    public int maxAtk;
+    public bool isOwner;
+    public bool canMove = true;
+    public bool canAttack;
+    public bool isBuilding;
+    public bool isRanger;
     public int currentHp;
     [HideInInspector] public int getDamage;
     [HideInInspector] public int currentAtk;
 
-    [SerializeField] public GameObject characterSprite;
+     public GameObject characterSprite;
 
     [Header("Detector")]
-    [SerializeField] public Detect detect;
-    [SerializeField] public float detectRange;
-    [SerializeField] public float attackRange;
-    [SerializeField] public LayerMask detectLayer;
-    [SerializeField] public float timeBtwHit;
+    public Detect detect;
+    public float detectRange;
+    public float attackRange;
+    public LayerMask detectLayer;
+    public float timeBtwHit;
     [HideInInspector] public float timeBtwHitCD;
     [HideInInspector] public Transform closetEnemy;
 
     [Header("UI")]
-    [SerializeField] public Slider healthBar;
-    [SerializeField] public GameObject healthBarCanvas;
+     public Slider healthBar;
+     public GameObject healthBarCanvas;
 
     [HideInInspector]
     public Camera cam;
